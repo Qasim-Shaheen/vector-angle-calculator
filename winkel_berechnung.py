09f575d3e5e64f91c43793247511a7e17ea87769
Qasim-Shaheen <qasim.shaheen.sh@gmail.com>
2026-01-03 00:07:43 +0100

    Update input prompts for vector components

--- winkel_berechnung.py
+++ winkel_berechnung.py
@@ -8,15 +8,15 @@ def calculate_angle(a,b):
     angle_degree = np.degrees(angle) # Wandelt den winkel von rad zu degree 
     return angle_degree
 
-vector_a1 = float(input("Vector a (x-value): "))
-vector_a2 = float(input("Vector a (y-value): "))
-vector_a3 = float(input("Vector a (z-value): "))
+vector_a1 = float(input("Enter x-component for Vector A: "))
+vector_a2 = float(input("Enter y-component for Vector A: "))
+vector_a3 = float(input("Enter z-component for Vector A: "))
 vector_a = np.array([vector_a1,vector_a2,vector_a3])
 print("")
 
-vector_b1 = float(input("Vector b (x-value): "))
-vector_b2 = float(input("Vector b (y-value): "))
-vector_b3 = float(input("Vector b (z-value): "))
+vector_b1 = float(input("Enter x-component for Vector B: "))
+vector_b2 = float(input("Enter y-component for Vector B: "))
+vector_b3 = float(input("Enter z-component for Vector B: "))
 vector_b = np.array([vector_b1,vector_b2,vector_b3])
     
 print("\n The angle between the vectors is :", calculate_angle(vector_a,vector_b),"degree")
